fbfd631f3b2d9141f879425df4c09e3d33ad5888
robtrivi <robtrivi@espol.edu.ec>
2022-12-01 23:14:44 -0500

    Cambios de estilos

--- ProyectoEDD01/VideogameStore/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.grupo02.videogamestore {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.grupo02.videogamestore to javafx.fxml;
-    exports com.grupo02.videogamestore;
-}
